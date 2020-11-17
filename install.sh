c5943ff4454e50bf2b67cf18a2b0db986de6bd36
Jan Quest <jan@quest-it.eu>
2020-11-17 22:55:20 +0100

    updated files

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
+source ./bin/copy_dotfile
+
 /Users/mfic/.dotfiles/vimrc/install_awesome_parameterized.sh /Users/mfic/.dotfiles/vimrc mfic
-ln -s /Users/mfic/.dotfiles/gitconfig /Users/mfic/.gitconfig
+
+copy_dotfile zshrc
