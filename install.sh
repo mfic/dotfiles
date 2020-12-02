639053d8721afd5e590102e6c6dce5daf83d1d38
Jan Quest <jan@quest-it.eu>
2020-12-02 21:15:17 +0100

    add tmux.conf file + update install

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ source ./bin/copy_dotfile
 /Users/mfic/.dotfiles/vimrc/install_awesome_parameterized.sh /Users/mfic/.dotfiles/vimrc mfic
 
 ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
