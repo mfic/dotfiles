e19734be983445199dd99be768c1c1cefc6da201
Jan Quest <jan@quest-it.eu>
2020-12-03 17:55:06 +0100

    update way how to call vimrc

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 source ./bin/copy_dotfile
 
-/Users/mfic/.dotfiles/vimrc/install_awesome_parameterized.sh /Users/mfic/.dotfiles/vimrc mfic
-
+ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
