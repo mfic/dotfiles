9780830ff239f75a3d1d44c6538ea379fcd0c9a6
Jan Quest <jan@quest-it.eu>
2021-02-05 00:09:59 +0100

    add option to perform actions based on OS

--- install.sh
+++ install.sh
@@ -1,5 +1,26 @@
+#!/usr/bin/env bash
 source ./bin/copy_dotfile
 
-ln -s ~/.dotfiles/vimrc ~/.vimrc
-ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
-ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+# Install softwareupdate
+# check current OS
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [ ${unameOut} == "Mac" ]; then
+    # Do something under Mac OS X platform
+elif [ ${unameOut} == "Linux" ]; then
+    # Do something under GNU/Linux platform
+elif [ ${unameOut} == "MinGw" ]; then
+    # Do something under 32 bits Windows NT platform
+fi
+# Symbolic links to config
+ln -sf ~/.dotfiles/vimrc ~/.vimrc
+ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/doom.d ~/.doom.d
