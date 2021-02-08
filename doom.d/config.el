f33f44e0849861b6f5ca2367a11e0edcb3954e41
Jan Quest <jan@quest-it.eu>
2021-02-08 22:18:33 +0100

    add org pretty, neotree and rss

--- doom.d/config.el
+++ doom.d/config.el
@@ -51,3 +51,7 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(setq
+    org-superstar-headline-bullets-list '("⁖" "◉" "○" "✸" "✿")
+)
