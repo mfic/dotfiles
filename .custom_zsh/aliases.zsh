58ddfdbbb767a60fdc910ea368fbb5c4eeef69a3
Jan Quest <jan@quest-it.eu>
2021-05-10 22:44:10 +0200

    add wpscan

--- .custom_zsh/aliases.zsh
+++ .custom_zsh/aliases.zsh
@@ -9,6 +9,7 @@ alias vi="vim";
 alias latex-build="docker run -i --rm --name latex -v "$PWD":/usr/src/app -w /usr/src/app registry.gitlab.com/islandoftex/images/texlive:latest arara -v";
 
 alias rustscan='docker run -it --rm --name rustscan rustscan/rustscan:latest';
+alias wpscan='docker run -it --rm wpscanteam/wpscan';
 
 if [[ -f "~/.custom_aliases" ]]; then
     source ~/.custom_aliases;
