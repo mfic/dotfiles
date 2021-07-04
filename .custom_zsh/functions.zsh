47a9e9f0265e5ff44a0363a0e892655f4642758d
Jan Quest <jan@quest-it.eu>
2021-07-04 21:48:51 +0200

    add function to rename all files in a folder sequentionally

--- .custom_zsh/functions.zsh
+++ .custom_zsh/functions.zsh
@@ -5,3 +5,7 @@ encodeb64 () {
 build-latex(){
     docker run -i --rm --name latex -v "$PWD":/usr/src/app -w /usr/src/app registry.gitlab.com/islandoftex/images/texlive:latest arara -v $1
 }
+
+rename-all-sequentially(){
+    ls -v | cat -n | while read n f; do mv -n "${f}" "${1}-${n}.${2}"; done 
+}
