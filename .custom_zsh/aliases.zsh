ed6f077c1f75504f599e1822b2e098cb6ec45acf
Jan Quest <jan@quest-it.eu>
2021-10-13 23:04:56 +0200

    add new function to rm terminating ns in k8s

--- .custom_zsh/aliases.zsh
+++ .custom_zsh/aliases.zsh
@@ -6,7 +6,7 @@ alias config="$GIT --git-dir=$HOME/.cfg/ --work-tree=$HOME";
 
 alias vi="vim";
 
-alias latex-build="docker run -i --rm --name latex -v "$PWD":/usr/src/app -w /usr/src/app registry.gitlab.com/islandoftex/images/texlive:latest arara -v";
+alias latex-build='docker run -i --rm --name latex -v $PWD:/usr/src/app -w /usr/src/app registry.gitlab.com/islandoftex/images/texlive:latest arara -v';
 
 alias rustscan='docker run -it --rm --name rustscan rustscan/rustscan:latest';
 alias wpscan='docker run -it --rm wpscanteam/wpscan';
--- .custom_zsh/functions.zsh
+++ .custom_zsh/functions.zsh
@@ -9,3 +9,14 @@ build-latex(){
 rename-all-sequentially(){
     ls -v | cat -n | while read n f; do mv -n "${f}" "${1}-${n}.${2}"; done 
 }
+
+kubectl_delte_ns()
+{
+    echo "Enter Namespace you want to delete:"
+    read NAMESPACE
+    kubectl get namespace $NAMESPACE -o json > $NAMESPACE.json
+    sed -i -e 's/"kubernetes"//' $NAMESPACE.json
+    kubectl replace --raw "/api/v1/namespaces/$NAMESPACE/finalize" -f ./$NAMESPACE.json
+    rm -rf ./$NAMESPACE.json
+    rm -rf ./$NAMESPACE.json-e
+}
