b4c4185bee61cb4ab26ac969f7dea5269c39d2be
Jan Quest <jan@quest-it.eu>
2022-01-18 23:15:40 +0100

    update laod of custom aliases

--- .custom_zsh/aliases.zsh
+++ .custom_zsh/aliases.zsh
@@ -1,6 +1,7 @@
 # various aliases
 
 GIT=$(which git);
+CUST_ALIASES=~/.custom_aliases
 
 alias config="$GIT --git-dir=$HOME/.cfg/ --work-tree=$HOME";
 
@@ -11,7 +12,7 @@ alias latex-build='docker run -i --rm --name latex -v $PWD:/usr/src/app -w /usr/
 alias rustscan='docker run -it --rm --name rustscan rustscan/rustscan:latest';
 alias wpscan='docker run -it --rm wpscanteam/wpscan';
 
-if [[ -f "~/.custom_aliases" ]]; then
-    source ~/.custom_aliases;
+if [[ -f "$CUST_ALIASES" ]]; then
+    source $CUST_ALIASES;
 fi
 
